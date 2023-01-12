0217c4740f89f7ea61753a64e939c162497769f3
Hojun1123 <kkkhhjjj@naver.com>
2023-01-12 23:23:36 +0900

    페이지별 권한, flash

--- Controller/user.py
+++ Controller/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, flash
 from Service.User import login
 
 userController = Blueprint("user", __name__, url_prefix="/")
@@ -12,8 +12,11 @@ def loginPage():  # put application's code here
         result = login.checkUser(request.form.get("id"), request.form.get("pwd"))
         if result:
             session['userid'] = result
-            return render_template("/main.html")
+            return "<script>location.href='/view/todayEngines';</script>"
+        else:
+            flash("로그인 정보가 올바르지 않습니다.")
+            return render_template("/login.html")
     else:
         if len(session) > 0:
-            return render_template("/main.html")
+            return "<script>alert('이미 "+session['userid']+" 계정으로 로그인 중 입니다.');location.href='/view/todayEngines';</script>"
         return render_template("/login.html")
