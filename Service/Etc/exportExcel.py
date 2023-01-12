f9a80abf4f356e997e7d56394ac5ecaefa3333ea
Hojun1123 <kkkhhjjj@naver.com>
2023-01-12 23:47:51 +0900

    코드 수정

--- Service/Etc/exportExcel.py
+++ Service/Etc/exportExcel.py
@@ -9,7 +9,7 @@ def engineDBToExcel():
         return -1
     try:
         tm = datetime.now()
-        fname = './입고' + str(tm.strftime("%Y%m%d%S")) + '.xlsx'
+        fname = './입고' + str(tm.strftime("%Y%m%d%S"))+'_'+str(tm.strftime("%H%M%S")) + '.xlsx'
         result = pd.DataFrame(data=result, columns=["바코드", "MIP", "TYPE", "입고일", "포장일", "Location", "GroupId", "불량", "비고란"])
         result.to_excel(excel_writer=fname, sheet_name='입고된 엔진', index=False)
     except Exception as e:
@@ -23,7 +23,7 @@ def OutputEngineDBToExcel():
         return -1
     try:
         tm = datetime.now()
-        fname = './불출' + str(tm.strftime("%Y%m%d%S")) + '.xlsx'
+        fname = './불출' + str(tm.strftime("%Y%m%d"))+'_'+str(tm.strftime("%H%M%S")) + '.xlsx'
         result = pd.DataFrame(data=result, columns=["바코드", "MIP", "TYPE", "입고일", "포장일", "출고일", "불량", "비고란", "목적지"])
         result.to_excel(excel_writer=fname, sheet_name='출고된 엔진', index=False)
     except Exception as e:
