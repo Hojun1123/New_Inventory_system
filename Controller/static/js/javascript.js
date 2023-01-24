28323c6282037bd6e6fc7a04ea46e4d4f0ec4bc0
ParkDH0809 <znxn1237@gmail.com>
2023-01-24 18:14:02 +0900

    frontend 작업 수정 완료

--- Controller/static/js/javascript.js
+++ Controller/static/js/javascript.js
@@ -181,3 +181,9 @@ function filter_rows() {
     })
 }
 
+function clickCheck(target) {
+    document.querySelectorAll(`input[type=checkbox]`)
+        .forEach(el => el.checked = false);
+
+    target.checked = true;
+}
